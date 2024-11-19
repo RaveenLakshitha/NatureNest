9ef59e585d3c22d39b4de146dfef3af8ed0f3591
Raveen_lw <raveenlw44@gmail.com>
2024-11-19 12:26:57 +0530

    image url fix-2

--- src/Components/Pages/ProductListing.jsx
+++ src/Components/Pages/ProductListing.jsx
@@ -10,17 +10,17 @@ export default function ProductListing() {
             id:1,
             itemName:"Areca Palm",
             itemPrice:75,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Areca-Palm-1-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Areca-Palm-1-480x600.jpg"
         },{
             id:2,
             itemName:"Massangeana Corn Plant",
             itemPrice:110,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Dracaena-Mass-Cane-3G-3-cane-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Dracaena-Mass-Cane-3G-3-cane-480x600.jpg"
         },{
             id:3,
             itemName:"Cast Iron Plant",
             itemPrice:90,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/cast-iron-plant-1-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/cast-iron-plant-1-480x600.jpg"
         },]
     },{
         category:"Totem",
@@ -28,19 +28,19 @@ export default function ProductListing() {
             id:4,
             itemName:"Philodendron ‘Emerald Red’",
             itemPrice:85,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Philodendron-Red-Emerald-3G-Totem-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Philodendron-Red-Emerald-3G-Totem-480x600.jpg"
         },
             {
             id:5,
             itemName:"Rhaphidophora decursiva",
             itemPrice:91,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Rhaphidophora-decursiva-Totem-3G-500x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Rhaphidophora-decursiva-Totem-3G-500x600.jpg"
         },
             {
             id:6,
             itemName:"Philodendron ‘Silver Sword’",
             itemPrice:85,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Philodendron-Silver-Sword-3G-Totem-1-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Philodendron-Silver-Sword-3G-Totem-1-480x600.jpg"
         },]
     },{
         category:"Tree",
@@ -48,17 +48,17 @@ export default function ProductListing() {
             {id:7,
             itemName:"Fiddle Leaf Fig",
             itemPrice:191,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Ficus-Lyrata-7G-STD-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest//images/Ficus-Lyrata-7G-STD-480x600.jpg"
         },
             {id:8,
             itemName:"Ficus benjamina ‘Wintergreen’",
             itemPrice:85,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Ficus-Wintergreen-3G-STD-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Ficus-Wintergreen-3G-STD-480x600.jpg"
         },
             {id:9,
             itemName:"Compact Dwarf Umbrella Tree",
             itemPrice:37,
-            itemImage:"https://github.com/RaveenLakshitha/NatureNest.git/images/Schefflera-MEdium-Compacta-tree-480x600.jpg"
+            itemImage:"https://raveenlakshitha.github.io/NatureNest/images/Schefflera-MEdium-Compacta-tree-480x600.jpg"
         },
     ]
     }    
